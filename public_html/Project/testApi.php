1f0e0ae3a3f51a3ebfde7d9720f30f0359b39e81
Adriel <ajz27@njit.edu>
2024-07-30 18:35:22 -0400

    experimentation

--- public_html/Project/testApi.php
+++ public_html/Project/testApi.php
@@ -2,28 +2,59 @@
 require(__DIR__ . "/../../partials/nav.php");
 
 $result = [];
-if (isset($_GET["symbol"])) {
-    //function=GLOBAL_QUOTE&symbol=MSFT&datatype=json
-    $data = ["function" => "GLOBAL_QUOTE", "symbol" => $_GET["symbol"], "datatype" => "json"];
-    $endpoint = "https://alpha-vantage.p.rapidapi.com/query";
+if (isset($_GET["term"])) {
+    // Extract parameters from the GET request
+    $term = $_GET["term"];
+    $locale = isset($_GET["locale"]) ? $_GET["locale"] : "en-US"; // Default locale to "en-US"
+    $offset = isset($_GET["offset"]) ? intval($_GET["offset"]) : 0; // Default offset to 0
+    $limit = isset($_GET["limit"]) ? intval($_GET["limit"]) : 5; // Default limit to 5
+
+    $data = [
+        "term" => $term,
+        "locale" => $locale,
+        "offset" => $offset,
+        "limit" => $limit
+    ];
+    $endpoint = "https://shazam.p.rapidapi.com/search";
     $isRapidAPI = true;
-    $rapidAPIHost = "alpha-vantage.p.rapidapi.com";
+    $rapidAPIHost = "shazam.p.rapidapi.com";
+
+    // Use a function similar to get() for API requests
     $result = get($endpoint, "STOCK_API_KEY", $data, $isRapidAPI, $rapidAPIHost);
-    //example of cached data to save the quotas, don't forget to comment out the get() if using the cached data for testing
-    /* $result = ["status" => 200, "response" => '{
-    "Global Quote": {
-        "01. symbol": "MSFT",
-        "02. open": "420.1100",
-        "03. high": "422.3800",
-        "04. low": "417.8400",
-        "05. price": "421.4400",
-        "06. volume": "17861855",
-        "07. latest trading day": "2024-04-02",
-        "08. previous close": "424.5700",
-        "09. change": "-3.1300",
-        "10. change percent": "-0.7372%"
-    }
-}'];*/
+
+    // Example of cached data to save the results, uncomment if testing with cached data
+    /*
+    $result = [
+        "status" => 200,
+        "response" => '{
+            "tracks": {
+                "hits": [
+                    {
+                        "track": {
+                            "title": "Track Title 1",
+                            "subtitle": "Artist Name",
+                            "images": {
+                                "coverart": "https://example.com/image1.jpg"
+                            },
+                            "key": "Track Key 1"
+                        }
+                    },
+                    {
+                        "track": {
+                            "title": "Track Title 2",
+                            "subtitle": "Artist Name",
+                            "images": {
+                                "coverart": "https://example.com/image2.jpg"
+                            },
+                            "key": "Track Key 2"
+                        }
+                    }
+                ]
+            }
+        }'
+    ];
+    */
+    
     error_log("Response: " . var_export($result, true));
     if (se($result, "status", 400, false) == 200 && isset($result["response"])) {
         $result = json_decode($result["response"], true);
@@ -33,24 +64,34 @@ if (isset($_GET["symbol"])) {
 }
 ?>
 <div class="container-fluid">
-    <h1>Stock Info</h1>
-    <p>Remember, we typically won't be frequently calling live data from our API, this is merely a quick sample. We'll want to cache data in our DB to save on API quota.</p>
+    <h1>Top Tracks</h1>
+    <p>Fetch the top tracks from Shazam based on your search criteria.</p>
     <form>
         <div>
-            <label>Symbol</label>
-            <input name="symbol" />
-            <input type="submit" value="Fetch Stock" />
+            <label>Search Term</label>
+            <input name="term" />
+            <label>Locale</label>
+            <input name="locale" value="en-US" />
+            <label>Offset</label>
+            <input name="offset" type="number" value="0" />
+            <label>Limit</label>
+            <input name="limit" type="number" value="5" />
+            <input type="submit" value="Fetch Tracks" />
         </div>
     </form>
-    <div class="row ">
-        <?php if (isset($result)) : ?>
-            <?php foreach ($result as $stock) : ?>
-                <pre>
-                    <?php var_export($stock);?>
-                </pre>
+    <div class="row">
+        <?php if (isset($result) && isset($result['tracks']['hits'])) : ?>
+            <?php foreach ($result['tracks']['hits'] as $track) : ?>
+                <div class="track">
+                    <h2><?php echo htmlspecialchars($track['track']['title']); ?></h2>
+                    <p><?php echo htmlspecialchars($track['track']['subtitle']); ?></p>
+                    <p>Key: <?php echo htmlspecialchars($track['track']['key'] ?? 'N/A'); ?></p>
+                    <img src="<?php echo htmlspecialchars($track['track']['images']['coverart']); ?>" alt="Cover Art" />
+                </div>
             <?php endforeach; ?>
         <?php endif; ?>
     </div>
 </div>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
+?>
